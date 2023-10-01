0cd3684df99f01c93a51d7260d3f049d38dfdfb1
hammadkhanwazir <insafstudentpti@gmail.com>
2023-10-01 12:24:21 +0500

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react'
 export default function Home() {
   return (
     <div>
-      <Suspense fallback={"loading"}>
+      <Suspense fallback={<p>loading... </p>}>
 
 
         <Hero /></Suspense>
