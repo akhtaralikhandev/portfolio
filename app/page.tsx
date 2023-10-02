a044ec12738661cf22dc2c0f2c838fa59f54fa85
hammadkhanwazir <insafstudentpti@gmail.com>
2023-10-02 20:19:06 +0500

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import Hero from '../components/hero/hero'
-import { Suspense } from 'react'
+import Hero from "../components/hero/hero";
+import { Suspense } from "react";
 // import Slider from '../components/slider/slider'
 export default function Home() {
   return (
     <div>
       <Suspense fallback={<p>loading... </p>}>
-
-
-        <Hero /></Suspense>
+        <Hero />
+      </Suspense>
     </div>
-
-
-  )
+  );
 }
